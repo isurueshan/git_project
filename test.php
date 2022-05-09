e890d5e0b88a7bad67fc4c276830f778def4a6c5
Isuru Eshan <isuru.eshan@gmail.com>
2022-05-09 23:22:51 +0100

    new page test.php interview array

--- test.php
+++ test.php
@@ -53,7 +53,7 @@ $i=0;
     $tr = in_array($string1,$wrdchnks);
     if($tr = true){
 echo $words[$i]."<br>";
-echo "found cat <br>";
+echo "found cat tess<br>";
     }else{
       echo "not found";
     }
